20e7a7ad3d5090774a20df075b3f7ea8d311f3c4
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-03-17 22:37:46 +0700

    Exercise 3.7

--- I0320102_exercise3.7.py
+++ I0320102_exercise3.7.py
@@ -1,1 +1,10 @@
+#Exercise 3.7
+#Contoh cara menghapus pada Dictionary python
 
+dict = {'Name':'Titus', 'Age':18, 'Class':'Fisrt'}
+del dict['Name']
+dict.clear()
+del dict
+
+print("dict['Age'] : ", dict['Age'])
+print("dict[School] :", dict['School'])
