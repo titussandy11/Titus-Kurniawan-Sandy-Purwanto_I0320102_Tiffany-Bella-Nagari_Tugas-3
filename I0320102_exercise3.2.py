996cb764d52dd4f334c732a2cf4656972ba5afbb
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-03-17 21:53:41 +0700

    Exercise 3.1

--- I0320102_exercise3.2.py
+++ I0320102_exercise3.2.py
@@ -1,1 +1,7 @@
+#Contoh cara update nilai pada python
 
+list = ['fisika', 'kimia', 1993, 2017]
+print('Nilai ada pada index 2 :', list[2])
+
+list[2] = 2001
+print('Nilai baru ada pada index 2 : ', list[2])
