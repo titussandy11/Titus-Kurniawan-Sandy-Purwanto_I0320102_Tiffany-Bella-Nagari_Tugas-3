0473d38f2948eeea357aa5d924f80b97c6fb4c6f
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-03-17 22:27:42 +0700

    Exercise 3.6

--- I0320102_exercise3.6.py
+++ I0320102_exercise3.6.py
@@ -1,1 +1,9 @@
+#Exercise 3.6
+#Update dictionary python
 
+dict = {'Name' : 'Titus', 'Age' : 18, 'Class' : 'First'}
+dict['Age'] = 20
+dict['School'] = "DPS School"
+
+print("dict['Age'] : ", dict['Age'])
+print("dict['School'] : ", dict['School'])
