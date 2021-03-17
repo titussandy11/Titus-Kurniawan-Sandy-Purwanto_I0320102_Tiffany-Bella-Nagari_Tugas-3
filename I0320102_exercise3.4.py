f6f33b0856a431af108bfc80161df9cd77157929
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-03-17 22:19:27 +0700

    Exercise 3.4

--- I0320102_exercise3.4.py
+++ I0320102_exercise3.4.py
@@ -1,1 +1,6 @@
+#Exercise 3.4
+#Contoh cara membuat Dictionary pada python
 
+dict = {'Name':'Titus', 'Age' : 18, 'Class' : 'First'}
+print("dict['Name'] : ", dict['Name'])
+print("dict['Age'] : ", dict['Age'])
