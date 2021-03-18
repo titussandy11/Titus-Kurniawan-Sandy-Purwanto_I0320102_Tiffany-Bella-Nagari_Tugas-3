cf1c720b7a35751651d52ec9223b82e9d63ad1bf
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-03-18 20:52:18 +0700

    Soal 2

--- I0320102_soal2_tugas3.py
+++ I0320102_soal2_tugas3.py
@@ -1,1 +1,25 @@
+#Soal 2
+#Titus Kurniawan Sandy Purwanto
+#I0320102
+#Kelas C
 
+dict = {'Nama' : ['Titus'],
+        'Hobi' : ['Bermain basket', 'tidur, makan'],
+        'Sosial media' : ['Instagram', 'line, Twitter'],
+        'Lagu kesukaan' : ['Save Your Tears', 'Loving is easy'],
+        'Makanan favorit' : ['Nasi goreng', 'soto', 'magelangan']}
+print(dict)
+
+#Ubah salah satu hobi & media sosial
+dict['Hobi'][0] = 'Menonton film'
+dict['Sosial media'][0] = ('Facebook')
+
+#Hapus dua makanan favorit
+del dict['Makanan favorit'][1:3]
+
+#Menambahkan satu hobi
+dict['Hobi'].append('Mendengarkan musik')
+
+
+
+print(dict)
