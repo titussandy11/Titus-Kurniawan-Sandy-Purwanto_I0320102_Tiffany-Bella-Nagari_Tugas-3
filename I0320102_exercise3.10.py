a6c05bdef6794e7f7e8e61568e6b6cdb180f6636
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-03-18 12:30:24 +0700

    Exercise 3.10

--- I0320102_exercise3.10.py
+++ I0320102_exercise3.10.py
@@ -1,1 +1,10 @@
+#Exercise 3.10
+#Menghapus nilai dalam tuple python
 
+tup = ('fisika', 'kimia', 1993, 2017)
+print(tup)
+
+del tup
+
+tup =('Bahasa', 'Literasi', 2020)
+print("Setelah menghapus tuple :", tup)
