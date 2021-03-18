85f74f2b855018a010b59a2d5607b6ded1481d05
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-03-18 13:26:21 +0700

    Soal 1

--- I0320102_soal1_tugas3.py
+++ I0320102_soal1_tugas3.py
@@ -1,1 +1,19 @@
+#Soal 1
+#Titus Kurniawan Sandy Purwanto
+#I0320102
+#Kelas C
 
+list = ['Issa', 'Nauval', 'Sekar', 'Harry', 'Tito', 'Soni', 'Ivan', 'Togar', 'Angga', 'Muhammad']
+print("Isi list pada index nomor [4, 6, 7] :", list[4], list[6], list[7])
+
+list[3] = 'Dias'
+list[5] = 'Albert'
+list[9] = 'Alex'
+
+list.append('David')
+list.append('Gagarin')
+
+print("Teman-temanku adalah : ")
+for temanku in list :
+    print(temanku)
+print("Jumlah dari temanku ada", len(list), "orang")
